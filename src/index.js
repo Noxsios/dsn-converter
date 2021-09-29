31c1d75d547ece8a0e92208fcb7083940e2204ad
Harry Randazzo <harry.randazzo@us.af.mil>
2021-09-29 13:40:01 -0400

    Add nonce key to emotion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./index.css";
 import "@elastic/eui/dist/eui_theme_dark.css";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
@@ -7,9 +9,16 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const cache = createCache({
+  key: "dsn-converter-emotion-cache-key",
+  nonce: "DIGITIZE_CSP_NONCE",
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CacheProvider value={cache}>
+      <App />
+    </CacheProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
