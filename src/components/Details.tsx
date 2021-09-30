e75debf79ce9ca230376b101b0b1da59615bbcd8
Harry Randazzo <harry.randazzo@us.af.mil>
2021-09-30 16:34:27 -0400

    Replace SVG w/ some fancy text manipulation, avoids CSP

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -26,10 +26,9 @@ const StyledDetails = styled.details`
       content: "";
     }
     &:after {
-      transform: translateY(-2px);
       float: right;
       transition: all 0.25s ease-in-out;
-      content: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg' class='euiIcon euiIcon--medium euiIcon-isLoaded eui-alignMiddle' fill='white' focusable='false' role='img' aria-hidden='true'%3E%3Cpath fill-rule='nonzero' d='M5.157 13.069l4.611-4.685a.546.546 0 000-.768L5.158 2.93a.552.552 0 010-.771.53.53 0 01.759 0l4.61 4.684c.631.641.63 1.672 0 2.312l-4.61 4.684a.53.53 0 01-.76 0 .552.552 0 010-.771z'%3E%3C/path%3E%3C/svg%3E");
+      content: "+";
     }
   }
   & .details-content {
@@ -37,8 +36,14 @@ const StyledDetails = styled.details`
   }
   &[open] {
     & summary::after {
+      display: inline-block;
       float: right;
-      transform: rotate(90deg) translateX(-3px);
+      transform: rotate(45deg);
+      border: 1px solid #1ba9f5;
+      width: 1.25rem;
+      height: 1.25rem;
+      border-radius: 50%;
+      text-align: center;
     }
     max-height: 100vh;
   }
