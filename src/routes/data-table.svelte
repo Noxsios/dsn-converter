a14854c6f803a5e9af3dec9e0b2c59a4e775670c
Harry Randazzo <harry@razzle.cloud>
2025-05-22 13:37:41 -0500

    filtering
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/routes/data-table.svelte
+++ src/routes/data-table.svelte
@@ -2,11 +2,15 @@
 	import {
 		type ColumnDef,
 		type PaginationState,
+		type ColumnFiltersState,
 		getCoreRowModel,
-		getPaginationRowModel
+		getPaginationRowModel,
+		getFilteredRowModel
 	} from '@tanstack/table-core';
 	import { createSvelteTable, FlexRender } from '$lib/components/ui/data-table/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Table from '$lib/components/ui/table/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
 
 	type DataTableProps<TData, TValue> = {
 		columns: ColumnDef<TData, TValue>[];
@@ -16,6 +20,7 @@
 	let { data, columns }: DataTableProps<TData, TValue> = $props();
 
 	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
+	let columnFilters = $state<ColumnFiltersState>([]);
 
 	const table = createSvelteTable({
 		get data() {
@@ -25,6 +30,9 @@
 		state: {
 			get pagination() {
 				return pagination;
+			},
+			get columnFilters() {
+				return columnFilters;
 			}
 		},
 		onPaginationChange: (updater) => {
@@ -34,11 +42,35 @@
 				pagination = updater;
 			}
 		},
+		onColumnFiltersChange: (updater) => {
+			if (typeof updater === 'function') {
+				columnFilters = updater(columnFilters);
+			} else {
+				columnFilters = updater;
+			}
+		},
 		getCoreRowModel: getCoreRowModel(),
-		getPaginationRowModel: getPaginationRowModel()
+		getPaginationRowModel: getPaginationRowModel(),
+		getFilteredRowModel: getFilteredRowModel()
 	});
 </script>
 
+<div class="flex items-center gap-2 py-4">
+	<Input
+		placeholder="e.g. Ramstein"
+		value={(table.getColumn('location')?.getFilterValue() as string) ?? ''}
+		onchange={(e) => {
+			table.getColumn('location')?.setFilterValue(e.currentTarget.value);
+		}}
+		oninput={(e) => {
+			table.getColumn('location')?.setFilterValue(e.currentTarget.value);
+		}}
+		class="w-full"
+	/>
+	<Button variant="outline" onclick={() => table.getColumn('location')?.setFilterValue('')}>
+		Clear
+	</Button>
+</div>
 <div class="rounded-md border">
 	<Table.Root>
 		<Table.Header>
@@ -73,4 +105,22 @@
 			{/each}
 		</Table.Body>
 	</Table.Root>
+</div>
+<div class="flex items-center justify-end space-x-2 py-4">
+	<Button
+		variant="outline"
+		size="sm"
+		onclick={() => table.previousPage()}
+		disabled={!table.getCanPreviousPage()}
+	>
+		Previous
+	</Button>
+	<Button
+		variant="outline"
+		size="sm"
+		onclick={() => table.nextPage()}
+		disabled={!table.getCanNextPage()}
+	>
+		Next
+	</Button>
 </div>
