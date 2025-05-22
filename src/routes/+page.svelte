ea929ce687cb8fce9597cfff6affcdfae22beb0e
Harry Randazzo <harry@razzle.cloud>
2025-05-22 14:39:38 -0500

    prettier
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,12 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as InputOTP from '$lib/components/ui/input-otp/index.js';
-	import DataTable from "./data-table.svelte";
-	import { columns } from "./columns";
+	import DataTable from './data-table.svelte';
+	import { columns } from './columns';
 
 	let query = $state('');
 
 	let base = $state<string[]>([]);
-
-	let isDisabled = $derived(base.length === 0);
 
 	function copy() {
 		navigator.clipboard.writeText(base.join(''));
@@ -36,7 +34,7 @@
 	});
 </script>
 
-<Card.Root class="mx-auto max-w-2xl">
+<Card.Root class="mx-2 my-4 max-w-2xl lg:mx-auto">
 	<Card.Header>
 		<Card.Title class="text-center text-2xl">DSN Converter</Card.Title>
 	</Card.Header>
@@ -59,13 +57,24 @@
 							</InputOTP.Group>
 						{/snippet}
 					</InputOTP.Root>
-					<Button variant="outline" size="lg" type="submit" href={telURI()} disabled={isDisabled}>
+				</div>
+			</div>
+
+			{#if base.length > 0}
+				<div class="flex gap-2 mt-4">
+					<Button class="cursor-pointer text-center flex-1" size="lg" variant="secondary" onclick={copy}>
+						<monospace class="text-amber-500"
+							>{base[0]} <span class="text-cyan-500">{base[1]}</span></monospace
+						>
+					</Button>
+					<Button variant="ghost" size="lg" type="submit" href={telURI()}>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
 							viewBox="0 0 16 16"
 							fill="currentColor"
+							class="fill-emerald-500 dark:fill-emerald-400"
 						>
 							<path
 								d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"
@@ -73,18 +82,8 @@
 						</svg>
 					</Button>
 				</div>
-			</div>
-
-			{#if base.length > 0}
-				<div class="mt-4 w-full text-center">
-					<Button class="w-full cursor-pointer" variant="outline" onclick={copy}>
-						<monospace class="text-amber-500"
-							>{base[0]} <span class="text-cyan-500">{base[1]}</span></monospace
-						>
-					</Button>
-				</div>
 			{/if}
-			<DataTable data={index} columns={columns} />
+			<DataTable data={index} {columns} />
 		</form>
 	</Card.Content>
 	<Card.Footer class="justify-center">
