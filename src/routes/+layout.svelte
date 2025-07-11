de88efbbb4046bd1c636d938c78582e02244f10e
Harry Randazzo <harry@razzle.cloud>
2025-07-11 15:37:47 -0500

    progress
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
 	<title>DSN Converter</title>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+	/>
 </svelte:head>
 
 <ModeWatcher />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import { search, index } from '$lib';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
 	import * as InputOTP from '$lib/components/ui/input-otp/index.js';
-	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
-	import * as Tabs from '$lib/components/ui/tabs/index.js';
-	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
 	import DataTable from './data-table.svelte';
 	import { columns } from './columns';
+	import { Separator } from '$lib/components/ui/separator/index.js';
 
 	let query = $state('');
 
@@ -37,72 +34,64 @@
 	});
 </script>
 
-<Card.Root class="mx-2 my-4 max-w-2xl lg:mx-auto">
-	<Card.Header>
-		<Card.Title class="text-center text-2xl">DSN Converter</Card.Title>
-	</Card.Header>
-	<Card.Content>
-		<Tabs.Root value="dialer">
-			<Tabs.List class="w-full">
-				<Tabs.Trigger value="dialer">Dial</Tabs.Trigger>
-				<Tabs.Trigger value="table">Table</Tabs.Trigger>
-			</Tabs.List>
-			<Tabs.Content value="dialer">
-				<form class="mt-4">
-					<div class="grid w-full items-center gap-4">
-						<div class="flex justify-center space-y-1.5 gap-x-2">
-							<InputOTP.Root maxlength={7} bind:value={query} pattern="[0-9]">
-								{#snippet children({ cells })}
-									<InputOTP.Group>
-										{#each cells.slice(0, 3) as cell}
-											<InputOTP.Slot {cell} />
-										{/each}
-									</InputOTP.Group>
-									<InputOTP.Separator />
-									<InputOTP.Group>
-										{#each cells.slice(3, 7) as cell}
-											<InputOTP.Slot {cell} />
-										{/each}
-									</InputOTP.Group>
-								{/snippet}
-							</InputOTP.Root>
-						</div>
-					</div>
+<div
+	class="mx-auto flex h-screen w-full max-w-[95vw] flex-col justify-between gap-4 py-4 md:max-w-4xl lg:max-w-5xl xl:max-w-6xl"
+>
+	<h4 class="text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">DSN Converter</h4>
+	<form>
+		<div
+			class="flex scale-100 justify-center sm:scale-125 md:my-12 md:scale-150 lg:my-16 lg:scale-175 xl:scale-200"
+		>
+			<InputOTP.Root
+				maxlength={7}
+				bind:value={query}
+				pattern="[0-9]"
+				aria-invalid={query.length >= 3 && base.length === 0}
+			>
+				{#snippet children({ cells })}
+					<InputOTP.Group>
+						{#each cells.slice(0, 3) as cell, i (i)}
+							<InputOTP.Slot {cell} />
+						{/each}
+					</InputOTP.Group>
+					<InputOTP.Separator />
+					<InputOTP.Group>
+						{#each cells.slice(3, 7) as cell, i (i + 3)}
+							<InputOTP.Slot {cell} />
+						{/each}
+					</InputOTP.Group>
+				{/snippet}
+			</InputOTP.Root>
+		</div>
 
-					{#if base.length > 0}
-						<div class="mt-4 flex gap-2">
-							<Button
-								class="flex-1 cursor-pointer text-center"
-								size="lg"
-								variant="secondary"
-								onclick={copy}
-							>
-								<monospace class="text-amber-500"
-									>{base[0]} <span class="text-cyan-500">{base[1]}</span></monospace
-								>
-							</Button>
-							<Button variant="ghost" size="lg" type="submit" href={telURI()}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									viewBox="0 0 16 16"
-									fill="currentColor"
-									class="fill-emerald-500 dark:fill-emerald-400"
-								>
-									<path
-										d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"
-									/>
-								</svg>
-							</Button>
-						</div>
-					{/if}
-				</form>
-			</Tabs.Content>
-			<Tabs.Content value="table"><DataTable data={index} {columns} /></Tabs.Content>
-		</Tabs.Root>
-	</Card.Content>
-	<Card.Footer class="justify-center">
-		<small class="text-sm text-orange-500">(former) SSgt Harry "razzle" Randazzo</small>
-	</Card.Footer>
-</Card.Root>
+		<!-- {#if base.length > 0} -->
+		<div class="flex gap-4">
+			<Button
+				class="flex-1 cursor-pointer text-center text-5xl"
+				size="lg"
+				variant="secondary"
+				onclick={copy}
+			>
+				<pre class="text-amber-500">{base[0]} <span class="text-cyan-500">{base[1]}</span></pre>
+			</Button>
+			<Button variant="ghost" size="lg" type="submit" href={telURI()}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					viewBox="0 0 16 16"
+					fill="currentColor"
+					class="fill-emerald-500 dark:fill-emerald-400"
+				>
+					<path
+						d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"
+					/>
+				</svg>
+			</Button>
+		</div>
+		<!-- {/if} -->
+	</form>
+	<Separator class="my-4" />
+	<DataTable data={index} {columns} />
+	<small class="text-sm text-orange-500">(former) SSgt Harry "razzle" Randazzo</small>
+</div>
